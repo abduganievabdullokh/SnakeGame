416537efcb342915989e4d595c3e641497b9d9dc
Abdullokh <99742107+abduganievabdullokh@users.noreply.github.com>
2022-05-24 23:20:59 +0500

    commit

---
+++ button.py
@@ -0,0 +1,29 @@
+# class Button file
+import pygame as p
+
+
+class Button:
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = p.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        # making mouse
+        position = p.mouse.get_pos()
+
+        # get the collision and click
+        if self.rect.collidepoint(position):
+            if p.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        if p.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        # displaying buttons on the screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        return action
--- main.py
+++ main.py
@@ -1,92 +1,38 @@
 import pygame as p
+import button as but
 import time
 from pygame.locals import *
+import apple
+import snake
 import random
 from tkinter import *
 
+
 SIZE = 40
-#BACKGROUND_COLOR = (200, 200, 200)
+SCREEN_HEIGHT = SIZE * 16
+SCREEN_WIDTH = SIZE * 30
 
 
-class Apple:
-    def __init__(self, parent_screen):
-        self.image = p.image.load("Properties/mushroom_mario.jpg").convert()
-        self.parent_screen = parent_screen
-        self.x = SIZE * 4
-        self.y = SIZE * 5
+screen = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+p.display.set_caption("Button screen")
 
-    def draw(self):
-        self.parent_screen.blit(self.image, (self.x, self.y))
-        p.display.flip()
+start_img = p.image.load('Properties/apple.jpg').convert_alpha()
+exit_img = p.image.load('Properties/block.jpg').convert_alpha()
 
-    def move(self):
-        self.x = random.randint(0, 29) * SIZE
-        self.y = random.randint(0, 15) * SIZE
-
-
-class Snake:
-    def __init__(self, parent_screen, length):
-        self.length = length
-        self.parent_screen = parent_screen
-        self.block = p.image.load("Properties/mario_block.jpg").convert()  # uploading image
-        self.x = [SIZE] * length  # place in x line
-        self.y = [SIZE] * length  # place in y line
-        self.direction = "up"
-        self.direction = "down"
-        self.direction = "left"
-        self.direction = "right"
-
-    def increase_length(self):
-        self.length += 1
-        self.x.append(-1)
-        self.y.append(-1)
-
-    def draw(self):
-        # self.parent_screen.fill(BACKGROUND_COLOR)
-
-        for i in range(self.length):
-            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
-        p.display.flip()
-
-    def move_left(self):
-        self.direction = "left"
-
-    def move_right(self):
-        self.direction = "right"
-
-    def move_up(self):
-        self.direction = "up"
-
-    def move_down(self):
-        self.direction = "down"
-
-    def walk(self):
-        for i in range(self.length - 1, 0, -1):
-            self.x[i] = self.x[i - 1]
-            self.y[i] = self.y[i - 1]
-
-        if self.direction == "up":
-            self.y[0] -= SIZE
-        if self.direction == "down":
-            self.y[0] += SIZE
-        if self.direction == "left":
-            self.x[0] -= SIZE
-        if self.direction == "right":
-            self.x[0] += SIZE
-        self.draw()
-
+start_button = but.Button(140, 210, start_img, 0.6)
+exit_button = but.Button(450, 210, exit_img, 0.6)
 
 class Game:
     def __init__(self):
         p.init()
         # p.display.set_caption("Snake Game")
-        #p.mixer.init()
-        self.play_backgroun_music()
-        self.surface = p.display.set_mode((SIZE * 30, SIZE * 16))  # width and height size
-        #self.surface.fill(BACKGROUND_COLOR)  # color of background
-        self.snake = Snake(self.surface, 3)
+        # p.mixer.init()
+        self.play_background_music()
+        self.surface = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # width and height size
+        # self.surface.fill(BACKGROUND_COLOR)  # color of background
+        self.snake = snake.Snake(self.surface, 3)
         self.snake.draw()
-        self.apple = Apple(self.surface)
+        self.apple = apple.Apple(self.surface)
         self.apple.draw()
 
     def is_collision(self, x1, y1, x2, y2):
@@ -95,7 +41,7 @@ class Game:
                 return True
         return False
 
-    def play_backgroun_music(self):
+    def play_background_music(self):
         p.mixer.music.load("Properties/super_mario_10_minutes.mp3")
         p.mixer.music.play()
 
@@ -127,9 +73,10 @@ class Game:
 
         # colliding with itself
         for i in range(1, self.snake.length):
-            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
-                self.play_sound("dead_mario")
-                raise "Game over"
+            pass
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+            self.play_sound("dead_mario")
+            raise "Game over"
 
     def display_score(self):
         font = p.font.SysFont("cambria", 45)
@@ -147,13 +94,18 @@ class Game:
         p.mixer.music.pause()
 
     def reset(self):
-        self.snake = Snake(self.surface, 1)
-        self.apple = Apple(self.surface)
+        self.snake = snake.Snake(self.surface, 1)
+        self.apple = apple.Apple(self.surface)
 
     def run(self):
         running = True
         pause = False
         while running:
+            screen.fill((111, 111, 111))
+            if start_button.draw(screen):
+                print('Start')
+            if exit_button.draw(screen):
+                running = False
             for event in p.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
@@ -182,10 +134,9 @@ class Game:
                 self.show_game_over()
                 pause = True
                 self.reset()
-            time.sleep(.248)
-
+            time.sleep(.1)
 
 
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    game_run = Game()
+    game_run.run()
